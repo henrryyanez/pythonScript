79180104a4c41870719c9d2343a167c7ae9fb03b
hy <henrryyanez@gmail.com>
2018-12-21 23:12:23 -0300

    Nueva versión de codigo,
    Falta integrar en aplicación web

--- repetidores_Tor.py
+++ repetidores_Tor.py
@@ -1,7 +1,8 @@
 #script para traer ip addresses de repetidores tor utilizando la libreria stem
-
+from colorama import init, Fore, Back, Style
 from stem.descriptor.remote import DescriptorDownloader
 import re
+init()
 cont = 0
 infoCompleta = ""
 downloader = DescriptorDownloader()
@@ -9,18 +10,33 @@ for descriptor in downloader.get_consensus().run():
   if descriptor.exit_policy.is_exiting_allowed():
     ipFind = re.findall( r'[0-9]+(?:\.[0-9]+){3}', str(descriptor))
     ipFind.pop(1)
-    
-    infoCompleta = infoCompleta + "\n" + str(ipFind)
+    parse = "".join(ipFind)
+    infoCompleta = infoCompleta + "\n" + str(parse)
     cont = cont + 1
-print (r"+------------------------------+")
-print (r"|     IP DE LOS NODOS          |")
-print (r"+------------------------------+")
+#print(ipFind, parse, cont)
+    
+print("---------Todos los Repetidores TOR------------")
 print (infoCompleta)
+print ("\nExisten: " + str(cont) + " nodos tor actualmente\n")
+try:
+  f = open("export.txt", "a")
+  f.write(infoCompleta)
+  f.close
+  print ("Se guardaron los datos en el archivo txt\n")
+except:
+  print ("no se pudo generar archivo txt\n")
+  
+search = input("Buscar IP? (y o n): ")
+if search == "y":
+  ip = input("Ip a buscar(EJ:10.0.0.0): ")
+  c = infoCompleta.split()
+  #print(c)
+  try:
+    b=c.index(ip)
+    print(Fore.RED + ip, "es un Tor Exit Node\n")
+  except ValueError:
+    print(Fore.GREEN + ip, "No es Tor Exit Node\n")
+else:
+  print(Fore.WHITE + "Cerrando aplicación...\n")
 
-print (r"+                         +")
-print (r"+-------------------------+")
-print (r"| CONTEO DE NODOS ACTIVOS |")
-print (r"+-------------------------+")
-print ("Existen: " + str(cont) + " nodos tor")
-print (r"+-------------------------+")
-print (r"+                         +")
+print(Fore.WHITE + "Proceso finalizado")
