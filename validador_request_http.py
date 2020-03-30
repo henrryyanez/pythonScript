dca099ca544994abfc1da4195c67978040bc7823
hy <henrryyanez@gmail.com>
2020-03-29 23:52:10 -0300

    Manejando el error

--- validador_request_http.py
+++ validador_request_http.py
@@ -2,7 +2,10 @@ import requests
 import time
 
 while True:
-    response = requests.get('https://tramitesadistancia.gob.ar/')
-    result = response.status_code
-    print(result)
-    time.sleep(3)
+    try:
+        response = requests.get('https://tramitesadistancia.gob.ar/')
+        result = response.status_code
+        print(result)
+        time.sleep(1)
+    except ConnectionError:
+        print("Falla en la respuesta...!!!")
