6b1fdc99321007cfccf57b342b672071f0ae8ddd
hy <henrryyanez@gmail.com>
2020-08-13 11:46:33 -0300

    Uno o más *

--- regexGroup.py
+++ regexGroup.py
@@ -1,6 +1,6 @@
 import re
 
-mensaje = 'Buscando numero telefonico 33-445566 oculto en 33-445-566 el text'
+mensaje = 'Buscando numero telefonico 445566 oculto en 33-445-566 el text de carararararacas'
 
 #Aplico expresion regular en texto sin quitar grupo
 phoneNumRegex = re.compile(r'\d{2}-\d{6}')
@@ -14,5 +14,10 @@ print(mo.group(3))
 
 #Patrones o grupos opcionales:
 phoneNumRegex = re.compile(r'(\d{2}-)?(\d{3}-\d{3})')
+mo = phoneNumRegex.search(mensaje)
+print(mo.group())
+
+#Patrones con uno o más...:
+phoneNumRegex = re.compile(r'ca(ra)*cas')
 mo = phoneNumRegex.search(mensaje)
 print(mo.group())
