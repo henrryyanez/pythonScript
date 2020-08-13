67a00b41adb4c64086498aa6e46f486b30088d87
hy <henrryyanez@gmail.com>
2020-08-13 11:23:23 -0300

    Agrupacion por partes

--- regexGroup.py
+++ regexGroup.py
@@ -1,6 +1,13 @@
 import re
 
-mensaje = 'Buscando numero telefonico 33-445566 oculto en el text'
+mensaje = 'Buscando numero telefonico 33-445566 oculto en 33-445-566 el text'
+
+#Aplico expresion regular en texto sin quitar grupo
 phoneNumRegex = re.compile(r'\d{2}-\d{6}')
 mo = phoneNumRegex.search(mensaje)
 print(mo.group())
+
+#Separo por grupos:
+phoneNumRegex = re.compile(r'(\d{2})-(\d{3})-(\d{3})')
+mo = phoneNumRegex.search(mensaje)
+print(mo.group(3))
